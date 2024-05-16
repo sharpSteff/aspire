f8a032dd2b8bb329062d374bcb3aefc2f8a00755
Stefan Kneidl <stefan.kneidl@alpsalpine.com>
2024-05-16 08:54:16 +0200

    fixed authorization

--- src/Aspire.Dashboard/Otlp/WebApplicationExtensions.cs
+++ src/Aspire.Dashboard/Otlp/WebApplicationExtensions.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Dashboard.Authentication;
 using Aspire.Dashboard.Otlp.Grpc;
 using Microsoft.AspNetCore.Mvc;
 using OpenTelemetry.Proto.Collector.Logs.V1;
@@ -14,14 +15,14 @@ public static class WebApplicationExtensions
     public static WebApplication ConfigureHttpOtlp(this WebApplication app, Uri httpEndpoint)
     {
         app.MapPost("/v1/metrics",
-            async ([FromServices] OtlpMetricsService service, HttpContext httpContext) =>
-            {
-                var body = await ReadFullyAsync(httpContext).ConfigureAwait(false);
-                var metrics = ExportMetricsServiceRequest.Parser.ParseFrom(body);
-                await service.Export(metrics, null!).ConfigureAwait(false);
-            }
-        ).AllowAnonymous()
-        .RequireHost($"*:{httpEndpoint.Port}");
+                async ([FromServices] OtlpMetricsService service, HttpContext httpContext) =>
+                {
+                    var body = await ReadFullyAsync(httpContext).ConfigureAwait(false);
+                    var metrics = ExportMetricsServiceRequest.Parser.ParseFrom(body);
+                    await service.Export(metrics, null!).ConfigureAwait(false);
+                }
+            ).RequireAuthorization(OtlpAuthorization.PolicyName)
+            .RequireHost($"*:{httpEndpoint.Port}");
 
         app.MapPost("/v1/traces",
                 async ([FromServices] OtlpTraceService service, HttpContext httpContext) =>
@@ -30,18 +31,18 @@ public static class WebApplicationExtensions
                     var traces = ExportTraceServiceRequest.Parser.ParseFrom(body);
                     await service.Export(traces, null!).ConfigureAwait(false);
                 }
-            ).AllowAnonymous()
+            ).RequireAuthorization(OtlpAuthorization.PolicyName)
             .RequireHost($"*:{httpEndpoint.Port}");
 
         app.MapPost("/v1/logs",
-            async ([FromServices] OtlpLogsService service, HttpContext httpContext) =>
-            {
-                var body = await ReadFullyAsync(httpContext).ConfigureAwait(false);
-                var logs = ExportLogsServiceRequest.Parser.ParseFrom(body);
-                await service.Export(logs, null!).ConfigureAwait(false);
-            }
-        ).AllowAnonymous()
-        .RequireHost($"*:{httpEndpoint.Port}");
+                async ([FromServices] OtlpLogsService service, HttpContext httpContext) =>
+                {
+                    var body = await ReadFullyAsync(httpContext).ConfigureAwait(false);
+                    var logs = ExportLogsServiceRequest.Parser.ParseFrom(body);
+                    await service.Export(logs, null!).ConfigureAwait(false);
+                }
+            ).RequireAuthorization(OtlpAuthorization.PolicyName)
+            .RequireHost($"*:{httpEndpoint.Port}");
 
         return app;
     }
